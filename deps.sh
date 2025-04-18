d41cda01dcc97e67728275442777ff1eb31bddc6
janetatomas1 <tomas.janeta@proton.me>
2025-04-18 12:53:25 +0200

    Project structure

--- deps.sh
+++
@@ -1,8 +0,0 @@
-
-rm third-party -rf
-
-conan install . --build=missing --output-folder=./third-party/gcc --deployer-folder=third-party/gcc --deployer=full_deploy --profile:all=$(pwd)/profiles/gcc
-
-conan install . --build=missing --output-folder=./third-party/clang --deployer-folder=third-party/clang --deployer=full_deploy --profile:all=$(pwd)/profiles/clang
-
-rm -f CMakeUserPresets.json
