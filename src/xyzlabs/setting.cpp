dba4f81a78a03a7d55b9737fdd4ad45c3e36de06
janetatomas1 <tomas.janeta@proton.me>
2025-10-28 11:56:36 +0100

    TextSetting

---
+++ src/xyzlabs/setting.cpp
@@ -0,0 +1,23 @@
+
+#include <imgui.h>
+#include <imgui_stdlib.h>
+#include <fmt/format.h>
+
+
+#include "xyzlabs/setting.hpp"
+
+void TextSetting::show(const std::string &label) {
+    ImGui::InputText(fmt::format("##{}", label_).c_str(), &value_);
+}
+
+std::unique_ptr<SettingInterface> TextSetting::clone() const {
+    return std::make_unique<TextSetting>(label_, value_);
+}
+
+void MultilineTextSetting::show(const std::string &label) {
+    ImGui::InputTextMultiline(fmt::format("##{}", label_).c_str(), &value_);
+}
+
+std::unique_ptr<SettingInterface> MultilineTextSetting::clone() const {
+    return std::make_unique<MultilineTextSetting>(label_, value_);
+}
