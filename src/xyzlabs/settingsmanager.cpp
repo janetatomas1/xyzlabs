3c73d060682dbc2e8c16265efb729bebfc3221f6
janetatomas1 <tomas.janeta@proton.me>
2025-10-29 12:05:51 +0100

    settings manager

--- src/xyzlabs/settingsmanager.cpp
+++ src/xyzlabs/settingsmanager.cpp
@@ -1,10 +1,75 @@
 
+#include <fstream>
+
 #include "xyzlabs/settingsmanager.hpp"
 #include "xyzlabs/globals.hpp"
 #include "xyzlabs/windowmanager.hpp"
 #include "xyzlabs/widget.hpp"
+#include "xyzlabs/xyzlabs.hpp"
 
-using namespace xyzlabs;
+namespace fs = std::filesystem;
+
+namespace xyzlabs {
+
+json merge(const json& a, const json& b) {
+    if (!a.is_object() || !b.is_object()) {
+        return a.is_null() ? b : a;
+    }
+
+    json result = a;
+
+    for (auto it = b.begin(); it != b.end(); ++it) {
+        const auto& key = it.key();
+        const auto& b_value = it.value();
+
+        if (result.contains(key)) {
+            result[key] = merge(result[key], b_value);
+        } else {
+            result[key] = b_value;
+        }
+    }
+
+    return result;
+}
+
+json load_json_safe(const std::string &file) {
+    json jv;
+    if(!fs::exists(fs::path(file))) {
+        std::fstream config(file, std::ios::in);
+        try {
+            config >> jv;
+        } catch(...) {}
+        config.close();
+    }
+
+    return jv;
+}
+
+void save_json(json jv, const std::string &file) {
+    std::fstream config(file, std::ios::out);
+    config << jv.dump(2);
+    config.close();
+}
+
+void save_safe(std::unique_ptr<SettingInterface> settings, const std::string &file) {
+    if(!fs::exists(fs::path(file).parent_path())) {
+        fs::create_directory(fs::path(file).parent_path());
+    }
+
+    auto jv0 = settings->to_json();
+    auto jv1 = load_json_safe(file);
+    auto merged = merge(jv0, jv1);
+    save_json(merged, file);
+}
+
+template <typename Derived, typename Base>
+std::unique_ptr<Derived> dynamic_unique_cast(std::unique_ptr<Base>&& base) {
+    if (auto ptr = dynamic_cast<Derived*>(base.get())) {
+        base.release();
+        return std::unique_ptr<Derived>(ptr);
+    }
+    return nullptr;
+}
 
 class SettingsWidget: public Widget {
     std::unique_ptr<SettingInterface> mainGroup_;
@@ -29,10 +94,16 @@ public:
     ImGui::EndChild();
 
     ImGui::SetCursorPos(discardBtnPos);
-    if(ImGui::Button("Discard changes", saveBtnSize)) {}
+    if(ImGui::Button("Discard changes", saveBtnSize)) {
+        window_manager().get_current_window()->close();
+    }
 
     ImGui::SetCursorPos(saveBtnPos);
-    if(ImGui::Button("Save changes", saveBtnSize)) {}
+    if(ImGui::Button("Save changes", saveBtnSize)) {
+        auto group = dynamic_unique_cast<SettingsGroup, SettingInterface>(std::move(mainGroup_));
+        settings_manager().receive_settings(std::move(group));
+        window_manager().get_current_window()->close();
+    }
 }
 
 SettingInterface* SettingsGroup::get_child(const std::string &path) {
@@ -74,7 +145,7 @@ SettingInterface* SettingsGroup::add_child(const std::string &path, std::unique_
 }
 
 json SettingsGroup::to_json() const {
-    json jv;
+    json jv = json::object();
     for(const auto &[key, value]: settings_) {
         jv[key] = value->to_json();
     }
@@ -138,12 +209,10 @@ SettingInterface* SettingsGroup::add_setting(const std::string &path, std::uniqu
     return add_child(path, std::move(setting));
 }
 
-void SettingsManager::init() {
-    mainGroup_ = std::make_unique<SettingsGroup>();
-}
-
-SettingInterface* SettingsManager::add_setting(const std::string &path, std::unique_ptr<SettingInterface> ptr) {
-    return mainGroup_->add_setting(path, std::move(ptr));
+SettingInterface* SettingsManager::add_setting(const std::string &path, std::unique_ptr<SettingInterface> setting) {
+    auto ptr = mainGroup_->add_setting(path, std::move(setting));
+    load_safe();
+    return ptr;
 }
 
 void SettingsManager::open_settings(int32_t width, int32_t height) {
@@ -159,4 +228,40 @@ void SettingsManager::open_settings(int32_t width, int32_t height) {
         std::unique_ptr<Widget> widget = std::make_unique<SettingsWidget>(std::move(mainGroup_->clone()));
         window->set_central_widget(std::move(widget));
     }
+}
+
+void SettingsManager::receive_settings(std::unique_ptr<SettingsGroup> group) {
+    save_safe(std::move(group), config_file());
+}
+
+std::string SettingsManager::config_file() {
+    return (app().app_directory() / "config.json").string();
+}
+
+void SettingsManager::init() {
+    mainGroup_ = std::make_unique<SettingsGroup>();
+    load_safe();
+}
+
+void SettingsManager::save() {
+    std::fstream config(config_file(), std::ios::out);
+    config << mainGroup_->to_json().dump(2);
+    config.close();
+}
+
+void SettingsManager::load() {
+    json jv;
+    std::fstream config(config_file(), std::ios::in);
+    config >> jv;
+    mainGroup_->from_json(jv);
+    config.close();
+}
+
+void SettingsManager::load_safe() {
+    if(!std::filesystem::exists(config_file())) {
+        save_safe(std::move(mainGroup_->clone()), config_file());
+    }
+
+    load();
+}
 }
