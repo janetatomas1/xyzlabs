99bf106d5c696b663a54c2f10a242c16b78e78f2
janetatomas1 <tomas.janeta@proton.me>
2025-11-09 15:40:56 +0100

    ImVec2 utils

--- src/xyzlabs/widget.cpp
+++ src/xyzlabs/widget.cpp
@@ -21,4 +21,24 @@ void Widget::close() {
     open_ = false;
 }
 
+ImVec2 operator*(const ImVec2 &first, const ImVec2 &second) {
+    return { first.x * second.x, first.y * second.y };
+};
+
+ImVec2 operator*(const ImVec2 &first, float x ) {
+    return { first.x * x, first.y * x };
+};
+
+ImVec2 operator*(float x, const ImVec2 &first) {
+    return { first.x * x, first.y * x };
+};
+
+ImVec2 operator+(const ImVec2 &first, const ImVec2 &second) {
+    return { first.x + second.x, first.y + second.y };
+};
+
+ImVec2 operator-(const ImVec2 &first, const ImVec2 &second) {
+    return { first.x - second.x, first.y - second.y };
+};
+
 }
