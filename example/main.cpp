f826e8290cc2075f19ea1692ece7534afde733b8
janetatomas1 <tomas.janeta@proton.me>
2025-11-10 13:17:06 +0100

    style

--- example/main.cpp
+++ example/main.cpp
@@ -2,7 +2,13 @@
 #include "xyzlabs/globals.hpp"
 #include "burningforest.hpp"
 
+struct W: public xyzlabs::Widget {
+    void show(const ImVec2 &size, const ImVec2 &pos) {
+        ImGui::Button("abcddd", {200.0, 100.0});
+    }
+};
+
 int main() {
-    app().init<BurningForest>("Burning forest");
+    app().init<W>("Burning forest");
     return app().exec();
 }
