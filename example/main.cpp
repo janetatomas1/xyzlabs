7f52ae42a1aff469431f34ccc12a5e9966d190c8
janetatomas1 <tomas.janeta@proton.me>
2025-11-10 15:23:19 +0100

    Custom first window

--- example/main.cpp
+++ example/main.cpp
@@ -2,13 +2,7 @@
 #include "xyzlabs/globals.hpp"
 #include "burningforest.hpp"
 
-struct W: public xyzlabs::Widget {
-    void show(const ImVec2 &size, const ImVec2 &pos) {
-        ImGui::Button("abcddd", {200.0, 100.0});
-    }
-};
-
 int main() {
-    app().init<W>("Burning forest");
+    app().init<Window, BurningForest>("Burning forest");
     return app().exec();
 }
