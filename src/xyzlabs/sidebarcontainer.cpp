72cbb6376148261dbbe7c4b2c0fc68d14eb39dfc
janetatomas1 <tomas.janeta@proton.me>
2025-11-28 15:44:16 +0100

    sidebarcontainer

--- src/xyzlabs/sidebarcontainer.cpp
+++ src/xyzlabs/sidebarcontainer.cpp
@@ -1,77 +1,90 @@
 
 #include "xyzlabs/sidebarcontainer.hpp"
+#include "xyzlabs/globals.hpp"
+#include "xyzlabs/eventmanager.hpp"
 
 namespace xyzlabs { 
+
+size_t idx(Edge e) {
+    return static_cast<size_t>(e);
+}
 
 void SidebarContainer::display(const ImVec2 &size, const ImVec2& position) {
     ImVec2 centralSize = size;
     ImVec2 centralPos = position;
     
-    if(leftSidebar_ != nullptr && leftSidebarOpen_) {
-        auto leftSidebarSize = size * leftSidebarSizeRelative_;
-        auto leftSidebarPos = size * leftSidebarPositionRelative_ + position;
+    auto &leftSidebar = sidebars_[idx(Edge::Left)];
+    auto &topSidebar = sidebars_[idx(Edge::Top)];
+    auto &rightSidebar = sidebars_[idx(Edge::Right)];
+    auto &bottomSidebar = sidebars_[idx(Edge::Bottom)];
+
+    if(leftSidebar.ptr != nullptr && leftSidebar.open) {
+        auto leftSidebarSize = size * leftSidebar.sizeRelative;
+        auto leftSidebarPos = size * leftSidebar.positionRelative + position;
 
         centralSize.x -= leftSidebarSize.x;
         centralPos.x += leftSidebarSize.x;
 
-        leftSidebar_->display(leftSidebarSize, leftSidebarPos);
+        leftSidebar.ptr->display(leftSidebarSize, leftSidebarPos);
     }
 
-    if(rightSidebar_ != nullptr && rightSidebarOpen_) {
-        auto rightSidebarSize = size * rightSidebarSizeRelative_;
-        auto rightSidebarPos = size * rightSidebarPositionRelative_ + position;
-
-        centralSize.x -= rightSidebarSize.x;
-
-        rightSidebar_->display(rightSidebarSize, rightSidebarPos);
-    }
-
-    if(header_ != nullptr && headerOpen_) {
-        auto headerSize = size * headerSizeRelative_;
-        auto headerPos = size * headerPositionRelative_ + position;
+    if(topSidebar.ptr != nullptr && topSidebar.open) {
+        auto headerSize = size * topSidebar.sizeRelative;
+        auto headerPos = size * topSidebar.positionRelative + position;
 
         centralSize.y -= headerSize.y;
         centralPos.y = headerSize.y;
 
-        header_->display(headerSize, headerPos);
+        topSidebar.ptr->display(headerSize, headerPos);
     }
 
-    if(footer_ != nullptr && footerOpen_) {
-        auto footerSize = size * footerSizeRelative_;
-        auto footerPos = size * footerPositionRelative_ + position;
+
+    if(rightSidebar.ptr != nullptr && rightSidebar.open) {
+        auto rightSidebarSize = size * rightSidebar.sizeRelative;
+        auto rightSidebarPos = size * rightSidebar.sizeRelative + position;
+
+        centralSize.x -= rightSidebarSize.x;
+
+        rightSidebar.ptr->display(rightSidebarSize, rightSidebarPos);
+    }
+
+    if(bottomSidebar.ptr != nullptr && bottomSidebar.open) {
+        auto footerSize = size * bottomSidebar.sizeRelative;
+        auto footerPos = size * bottomSidebar.positionRelative + position;
 
         centralSize.y -= footerSize.y;
-        footer_->display(footerSize, footerPos);
+        bottomSidebar.ptr->display(footerSize, footerPos);
     }
     
     Widget::display(centralSize, centralPos);
 }
 
+Widget* SidebarContainer::get_sidebar(Edge edge) {
+    return sidebars_[idx(edge)].ptr.get();
+}
+
 void SidebarContainer::set_sidebar(std::unique_ptr<Widget> widget, Edge edge) {
-    switch (edge) {
-        case Edge::Left: {
-            leftSidebar_ = std::move(widget);
-            leftSidebarOpen_ = true;
-            break;
-        };
-        case Edge::Right: {
-            rightSidebar_ = std::move(widget);
-            rightSidebarOpen_ = true;
-            break;
-        }
-        case Edge::Top: {
-            header_ = std::move(widget);
-            headerOpen_ = true;
-            break;
-        }
-        case Edge::Bottom: {
-            footer_ = std::move(widget);
-            footerOpen_ = true;
-            break;
-        }
-        default:
-            break;
-    }
+    action act = [this, widget = std::move(widget), edge]() mutable {
+        sidebars_[idx(edge)].ptr = std::move(widget);
+    };
+
+    event_manager().add_action(std::move(act));
+}
+
+ImVec2 SidebarContainer::get_sidebar_size_relative(Edge edge) {
+    return sidebars_[idx(edge)].sizeRelative;
+}
+
+ImVec2 SidebarContainer::get_sidebar_position_relative(Edge edge) {
+    return sidebars_[idx(edge)].positionRelative;
+}
+
+void SidebarContainer::set_sidebar_size_relative(const ImVec2 &size, Edge edge) {
+    sidebars_[idx(edge)].sizeRelative = size;
+}
+
+void SidebarContainer::set_sidebar_position_relative(const ImVec2 &pos, Edge edge) {
+    sidebars_[idx(edge)].positionRelative = pos;
 }
 
 }
