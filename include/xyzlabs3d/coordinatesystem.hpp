4dd32243bf50fe6a332f2a6f0c6caea74b84a03f
janetatomas1 <tomas.janeta@proton.me>
2026-01-02 22:55:49 +0100

    grid

--- include/xyzlabs3d/coordinatesystem.hpp
+++ include/xyzlabs3d/coordinatesystem.hpp
@@ -7,10 +7,17 @@
 namespace xyzlabs3d {
 
 struct CoordinateSystem: public Magnum::SceneGraph::Drawable3D {
-    Mesh mesh_;
-    VertexColorGL3D shader_;
+    Mesh axisMesh_;
+    Mesh gridMesh_;
+
+    VertexColorGL3D axisShader_;
+    VertexColorGL3D gridShader_;
+
+    bool showAxis_ = true;
+    bool showGrid_ = true;
+
 public:
-    CoordinateSystem(Object3D& object, DrawableGroup3D* drawables);
+    CoordinateSystem(Object3D& object, DrawableGroup3D* drawables, float radius = 1.0f, float gridStep = 0.1f);
     void draw(const Matrix4& transformation, Camera3D& camera);
 };
 
--- src/xyzlabs3d/coordinatesystem.cpp
+++ src/xyzlabs3d/coordinatesystem.cpp
@@ -1,17 +1,19 @@
 
-#include <Magnum/Math/Angle.h>
-
 #include "xyzlabs3d/coordinatesystem.hpp"
-
+#include <Magnum/Magnum.h>
 
 using namespace Magnum;
 
 namespace xyzlabs3d {
 using namespace Math::Literals;
 
-CoordinateSystem::CoordinateSystem(Object3D& object, DrawableGroup3D* drawables)
-    : Drawable3D(object, drawables) {
-    GL::Buffer vertexBuffer;
+CoordinateSystem::CoordinateSystem(
+    Object3D& object,
+    DrawableGroup3D* drawables,
+    float radius,
+    float gridStep
+): Drawable3D(object, drawables) {
+    GL::Buffer axisVertexBuffer;
 
     struct Vertex {
         Vector3 position;
@@ -20,30 +22,61 @@ CoordinateSystem::CoordinateSystem(Object3D& object, DrawableGroup3D* drawables)
 
     Vertex vertices[] = {
         Vertex{{0.0f, 0.0f, 0.0f}, 0xff0000_rgbf},
-        Vertex{{1.0f, 0.0f, 0.0f}, 0xff0000_rgbf},
+        Vertex{{radius, 0.0f, 0.0f}, 0xff0000_rgbf},
         Vertex{{0.0f, 0.0f, 0.0f}, 0x00ff00_rgbf},
-        Vertex{{0.0f, 1.0f, 0.0f}, 0x00ff00_rgbf},
+        Vertex{{0.0f, radius, 0.0f}, 0x00ff00_rgbf},
         Vertex{{0.0f, 0.0f, 0.0f}, 0x0000ff_rgbf},
-        Vertex{{0.0f, 0.0f, 1.0f}, 0x0000ff_rgbf}
+        Vertex{{0.0f, 0.0f, radius}, 0x0000ff_rgbf}
     };
 
-    vertexBuffer.setData(vertices, GL::BufferUsage::DynamicDraw);
-    mesh_
+    axisVertexBuffer.setData(vertices, GL::BufferUsage::DynamicDraw);
+    axisMesh_
         .setPrimitive(GL::MeshPrimitive::Lines)
-        .setCount(6)
+        .setCount(sizeof(vertices))
         .addVertexBuffer(
-            vertexBuffer,
+            axisVertexBuffer,
             0,
             Shaders::VertexColorGL3D::Position{},
             Shaders::VertexColorGL3D::Color3{}
         );
-    auto x = mesh_.count();
+
+    std::vector<Vertex> gridVertices;
+    float current = -radius;
+    while(current <= radius) {
+        gridVertices.push_back({{current, -radius, 0.0f}, 0xffffff_rgbf});
+        gridVertices.push_back({{current, radius, 0.0f}, 0xffffff_rgbf});
+        gridVertices.push_back({{radius, current, 0.0f}, 0xffffff_rgbf});
+        gridVertices.push_back({{-radius, current, 0.0f}, 0xffffff_rgbf});
+        current += gridStep;
+    }
+    GL::Buffer gridVertexBuffer;
+
+    Corrade::Containers::ArrayView<Vertex> vertexBufferView{&gridVertices[0], gridVertices.size()};
+    gridVertexBuffer.setData(vertexBufferView, GL::BufferUsage::StaticDraw);
+    gridMesh_
+        .setPrimitive(GL::MeshPrimitive::Lines)
+        .setCount(gridVertices.size())
+        .addVertexBuffer(
+            gridVertexBuffer,
+            0,
+            Shaders::VertexColorGL3D::Position{},
+            Shaders::VertexColorGL3D::Color3{}
+    );
 }
 
 void CoordinateSystem::draw(const Matrix4& transformation, SceneGraph::Camera3D& camera) {
-    shader_
-        .setTransformationProjectionMatrix(camera.projectionMatrix() * transformation)
-        .draw(mesh_);
+    auto transform = camera.projectionMatrix() * transformation;
+
+    gridShader_.setTransformationProjectionMatrix(transform);
+    axisShader_.setTransformationProjectionMatrix(transform);
+
+    if (showGrid_) {
+        gridShader_.draw(gridMesh_);
+    }
+
+    if (showAxis_) {
+        axisShader_.draw(axisMesh_);
+    }
 }
 
 }
